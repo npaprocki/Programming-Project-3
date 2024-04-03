5c08b96856b49fc849a2459eef96020b933a6e65
npaprocki <100364742+npaprocki@users.noreply.github.com>
2024-04-03 12:52:30 -0400

    finished, just needs tested

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * Class: Calculator
+ * @author natepaprocki
+ * @version 1.0
+ * Course: CSE 274 Spring 2024
+ * Written: April 5, 2024
+ * 
+ * Purpose: This class is intended to solve expressions that contain integers operands, basic operators,
+ * 			and multiple levels of parentheses.
+ * 
+ */
+
 public class Calculator {
 
-	//this method was modifed from first site
-	// Function to return precedence of operators
+	/**
+	 * Method to determine precedence of operators
+	 * 
+	 * Note:Tthis method was modifed from https://www.geeksforgeeks.org/convert-infix-expression-to-postfix-expression/
+	 * 
+	 * @param c	Passed Operand
+	 * @return returns 1 if plus or minus and returns 2 if division or multiplication
+	 */
 	public static int prec(char c) {
 
 		if (c == '/' || c == '*') {
@@ -22,21 +40,16 @@ public class Calculator {
 		}
 	}
 
-	//this method was copied from first site
-	// Function to return associativity of operators
-	public static char associativity(char c) {
-
-		if (c == '^') {
-
-			return 'R';
-
-		}
-
-		return 'L';
-	}
-
-	//this method was modifed from first site and starter code
-	public static String parse(String passedExpression) {
+	/**
+	 * Method to convert an infix expression to postfix expression
+	 * 
+	 * Note: This method was modifed from https://www.geeksforgeeks.org/convert-infix-expression-to-postfix-expression/
+	 * 
+	 * @param passedExpression	Passed infix expression
+	 * @return postfix expression
+	 */
+	
+	public static String convertPostFix(String passedExpression) {
 
 		StringBuilder result = new StringBuilder();
 
@@ -57,88 +70,182 @@ public class Calculator {
 					n = n * 10 + (int) (ch - '0');
 
 					j++;
-
+					
 					if (j < passedExpression.length()) {
 
 						ch = passedExpression.charAt(j);
 
 					} else {
 
-						// break looop
-						ch = 'a';
+						break;
 
 					}
+
 				}
 
+				//adjust value of j back 1 when char.At(j) is not a number
 				j--;
 
 				operand = n;
 
-				result.append(operand);
+				result.append(operand  + " ");
 
 			} else {
 				
+				//handle open parathesis
+
 				if (ch == '(') {
-					
+
 					stack.push(ch);
-				
+
 				} else {
+
+					//handle close parathesis
 					
 					if (ch == ')') {
-				
 
 						while (!stack.isEmpty() && stack.peek() != '(') {
-	
-							result.append(stack.pop());
-	
+
+							result.append(stack.pop() + " ");
+
 						}
-	
+
 						stack.pop(); // Pop '('
-				
+
 					} else {
+						
+						//handle operators
 
 						if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-							
-						
+
 							while (!stack.isEmpty() && (prec(ch) < prec(stack.peek())
-									|| prec(ch) == prec(stack.peek()) && associativity(ch) == 'L')) {
-								
-								result.append(stack.pop());
-							
+									|| prec(ch) == prec(stack.peek()))) {
+
+								result.append(stack.pop()  + " ");
+
 							}
 							stack.push(ch);
-						
+
+						}
+
 					}
 
+				}
+
 			}
-
-		}
-			
-		}
 		}
 
+		//output to string
 		while (!stack.isEmpty()) {
-			
+
 			result.append(stack.pop());
-			
+
 		}
 
 		return result.toString();
 
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * Method to evaulate a postfix expression
+	 * 
+	 * Note: This method was modifed from https://www.geeksforgeeks.org/evaluation-of-postfix-expression/
+	 * 
+	 * @param passedPostExpression passed postfix expression 
+	 * @return value of postfix expression
+	 */
+	
+	public static double evaulate(String passedPostExpression) {
+		
+		// Create a stack
+        Stack<Double> stack = new Stack<>();
+ 
+        // Scan all characters one by one
+        for (int i = 0; i < passedPostExpression.length(); i++) {
+            char c = passedPostExpression.charAt(i);
+ 
+            if (c == ' ')
+                continue;
+ 
+            // If the scanned character is an operand
+            // (number here),extract the number
+            // Push it to the stack.
+            else if (Character.isDigit(c)) {
+                
+            	int n = 0;
 
+
+				while (Character.isDigit(c)) {
+
+					n = n * 10 + (int) (c - '0');
+
+					i++;
+					
+					if (i < passedPostExpression.length()) {
+
+						c = passedPostExpression.charAt(i);
+
+					} else {
+
+						break;
+
+					}
+				
+				}
+
+                // Push the number in stack
+                stack.push((double) n);
+            }
+ 
+            // If the scanned character is an operator, pop
+            // two elements from stack apply the operator
+            else {
+                
+            	double val1 = stack.pop();
+            	double val2 = stack.pop();
+ 
+                switch (c) {
+                case '+':
+                    stack.push(val2 + val1);
+                    break;
+                case '-':
+                    stack.push(val2 - val1);
+                    break;
+                case '/':
+                    stack.push(val2 / val1);
+                    break;
+                case '*':
+                    stack.push(val2 * val1);
+                    break;
+                }
+            }
+        }
+        
+        return stack.pop();
+    }
+	
+	/**
+	 * Main method
+	 * 
+	 * @param args Command line argument, not used
+	 */
+	
+	public static void main(String[] args) {
+		//create scanner
 		Scanner input = new Scanner(System.in);
 
+		//prompt and take input
 		System.out.println("Enter Expression: ");
 
 		String expression = input.nextLine();
 
-		String postFixExp = parse(expression);
-
-		System.out.println(postFixExp);
-
+		//convert to postfix
+		String postFixExp = convertPostFix(expression);
+		
+		//evaulate postFix
+		System.out.println(expression + " = " + evaulate(postFixExp));
+		
+		//close scanner
 		input.close();
 
 	}
